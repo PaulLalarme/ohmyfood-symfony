8b5891baff27c87ef4c66030016a4380caed3ca0
Maradzony <lalarme.paul@gmail.com>
2023-11-30 13:27:23 +0100

    Add into database with RestaurantController.php

--- src/src/Controller/RestaurantController.php
+++ src/src/Controller/RestaurantController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Restaurant;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -44,5 +46,23 @@ class RestaurantController extends AbstractController
     public function connect(): Response
     {
         return $this->render('pages/connection.html.twig');
+    }
+
+    #[Route('/restaurant/new')]
+    public function new(EntityManagerInterface $entityManager): Response
+    {
+        $restaurant = new Restaurant();
+        $restaurant->setName('La palette du goût');
+        $restaurant->setAddress('1 Rue du Languedoc');
+        $restaurant->setThumbnail('LaPaletteDuGout');
+        $restaurant->setRate(5.0);
+            $entityManager->persist($restaurant); // surveille l'objet "$restaurant"
+            $entityManager->flush(); // utilise "EntityManagerInterface" pour enregistrer en base à partir de l'objet
+
+        return new Response(sprintf(
+           'Nom: %s | adresse: %s',
+            $restaurant->getName(),
+            $restaurant->getAddress()
+        )); // retourne un message
     }
 }
