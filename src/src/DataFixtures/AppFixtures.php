618eccb2c5ef5cd07950d12645f3b24874992701
Maradzony <lalarme.paul@gmail.com>
2023-12-06 10:21:15 +0100

    Create AppFixtures.php

---
+++ src/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $user = new User();
+        $user->setFirstName('Paul')
+            ->setEmail('lalarme.paul@gmail.com');
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
